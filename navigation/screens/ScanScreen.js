3c7724df18dd794fe2d84405d163305ed7da6e6a
Rohan Soma <rohansoma80@gmail.com>
2023-08-07 20:41:01 -0400

    Modifications to Screens

--- navigation/screens/ScanScreen.js
+++ navigation/screens/ScanScreen.js
@@ -1,30 +1,14 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
-import axios from 'axios';
-
-function fetchAPI() {
-  axios.get('http://localhost:5000/hello')
-    .then(response => console.log(response.data))
-}
-
-class App extends React.Component {
-  componentDidMount() {
-    fetchAPI();
-  }
-
-  render() {
-    return (
-      "test"
-    );
-  }
-}
-
 export default function ScanScreen({navigation}){
   return(
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text
       onPress = {() => navigation.navigate('Home')}
-      style = {{fontSize: 26, fontWeight: 'bold'}}>Fitness!</Text>
+      style = {{fontSize: 26, fontWeight: 'bold'}}>Scan!</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+});
