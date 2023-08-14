acc9fa63ba6be33af4e01e0464565e5712dcc69f
Rohan Soma <rohansoma80@gmail.com>
2023-08-14 11:18:12 -0400

    Changes to Profile

--- navigation/screens/ScanScreen.js
+++ navigation/screens/ScanScreen.js
@@ -16,7 +16,7 @@ export default function ScanScreen({navigation}){
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    alert('Bar code with type ${type} and data ${data} has been scanned!');
   };
 
   if (hasPermission === null)
@@ -28,7 +28,6 @@ export default function ScanScreen({navigation}){
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
       />
       {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
