4b902453831ab74383725fb7fa2e27c8482fdf74
Rohan Soma <rohansoma80@gmail.com>
2023-08-20 21:20:04 -0400

    Changes to Introduction

--- navigation/screens/WelcomeScreen.js
+++ navigation/screens/WelcomeScreen.js
@@ -1,33 +1,45 @@
 import * as React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import userData from '../Data';
+import { useState } from 'react';
+import { View, Text, TextInput, Button, Pressable, StyleSheet } from 'react-native';
+import Data from '../Data.json';
 
 export default function WelcomeScreen({navigation}){
+  const [ username, setUsername ] = useState(null);
+  const [ password, setPassword ] = useState(null);
+
   return(
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text
-      style = {{fontSize: 26, fontWeight: 'bold'}}>Welcome to Grass!</Text>
-      <Button
-        title = "Click Here to Create Your Profile"
-        onPress={() => {navigation.navigate('Set Data')}}
-        />
+      <Text style = {{fontSize: 26, fontWeight: 'bold', textAlign: 'center'}}>Account Setup</Text>
+      <Text style = {styles.text}>Username: </Text>
+      <TextInput style = {styles.textInput} value = {username} onChangeText = {setUsername} placeholder = "Username" returnKeyType="done"/>
+      <Text style = {styles.text}>Password: </Text>
+      <TextInput style = {styles.textInput} value = {password} onChangeText = {setPassword} placeholder = "Password" returnKeyType="done"/>
+      <Pressable style = {styles.button} onPress = {() => navigation.navigate('Set Data')}>
+        <Text style = {{fontWeight: 'bold', fondSize: 26, left: 10}}>Continue</Text>
+      </Pressable>
     </View>
   );
 };
 
 const styles = StyleSheet.create ({
-    text: {
-      fontSize: 10,
-      marginTop: 10,
-    },
-    button: {
-      borderRadius: 10,
-      backgroundColor: "#4A412A",
-      textAlign: 'center',
-    },
-    textInput: {
-      width: 40,
-      height: 20,
-      borderRadius: 10,
-    },
+  text: {
+    fontSize: 20,
+    margin: 10,
+  },
+  button: {
+    borderRadius: 5,
+    backgroundColor: '#86f7a0',
+    top: 20,
+    height: 40,
+    justifyContent: 'center',
+    width: 400,
+    textAlign: 'center',
+  },
+  textInput: {
+    width: 400,
+    height: 40,
+    margin: 25,
+    borderWidth: 2,
+    borderRadius: 5,
+  },
 });
